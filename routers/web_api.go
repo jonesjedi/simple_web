7d7b242c5bad08f634d38548092f832405d4b317
JD-kriswu <wuzepingladeng@126.com>
2020-12-19 23:31:02 +0800

    fix

--- routers/web_api.go
+++ routers/web_api.go
@@ -12,6 +12,9 @@ func InitApiRoute(router *gin.Engine) *gin.Engine {
 	appRouter.POST("/user/login", handler.HandleLoginRequest)
 	appRouter.GET("index/logout", handler.HandleTestRequest)
 	appRouter.POST("register", handler.HandleRegisteRequest)
+
+	appRouter.POST("/user/send_validate_email", handler.HandleSendValidateEmailRequest)
+	appRouter.GET("/user/validate_email", handler.HandleValidateEmailRequest)
 	// 后台服务路由
 	bkRouter := appRouter.Group("/").Use()
 	{
