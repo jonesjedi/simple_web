dd18a2c96c5ae994c5deda67caa5805a0701b5dd
Shuo Xu <jonesjedi@gmail.com>
2021-03-08 00:04:39 +0800

    fix error return

--- utils/htmlparser2/htmlparser2_test.go
+++ utils/htmlparser2/htmlparser2_test.go
@@ -7,7 +7,7 @@ import (
 
 // TestParseURL 测试生成URL预览
 func TestParseURL(t *testing.T) {
-	title, desc, img, err := ParseURL("https://www.163.com")
+	title, desc, img, err := ParseURL("https://detail.1688.com/offer/587028735680.html?spm=a2609.11209760.j661dm7m.6.44292de1umP81Z&tracelog=p4p&clickid=bbc6ba83b35a4657885c1d9f2766847c&sessionid=17cc5c787f511cef575df4749836baf1")
 	if err != nil {
 		fmt.Println(err)
 	}
